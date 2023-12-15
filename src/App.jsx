6b33165dbf0b16717e2f5a2589fed366b09f382c
Arsh <pawar.arsh1333@gmail.com>
2023-12-15 21:43:18 +0530

    pc version completed

--- src/App.jsx
+++ src/App.jsx
@@ -63,15 +63,56 @@ function App() {
         <hr />
       </div>
       <div className="news">
-        <ul>
-          {/*myData.map((i) => {
+        <h1 className="news-heading">Top News</h1>
+        {/*<div className="card">
+          <div className="card-img">
+            <img src="" alt="" />
+          </div>
+          <div className="p-card">Lorem ipsum dolor sit amet.</div>
+        </div>*/}
+        {/*<ul>
+          {myData.map((i) => {
             return (
               <li key={i.title}>
-                <img src={i.urlToImage} alt="" />
+                <div className="display-li">
+                  <div className="card">
+                    <div className="card-img">
+                      <img height="80px" src={i.urlToImage} alt="" />
+                    </div>
+                    <div className="p-card">{i.title}</div>
+                  </div>
+                </div>
               </li>
             );
-          })*/}
-        </ul>
+          })}
+        </ul>*/}
+        <div className="display">
+          {myData.map((i) => {
+            return (
+              <div className="display-content">
+                <div className="content-text">
+                  <h3 className="p-display-main">{i.title}</h3>
+                  <p className="p-display">{i.author}</p>
+                  <p className="p-display">{i.source.name}</p>
+                  <p className="p-display">{i.content}</p>
+                  <p className="read">
+                    <a href={i.url}>Read More</a>
+                  </p>
+                </div>
+                <div className="content-image">
+                  <img
+                    className="content-img"
+                    src={
+                      i.urlToImage ||
+                      "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAKEAAACUCAMAAADMOLmaAAAAMFBMVEXp7vG6vsHs8fTBxcjl6u28wcTEyMve4+bM0NO1uLu3u77Q1NfY3eDh5unT2Nvu9PdnWQIZAAABxElEQVR4nO3Z63KrIBRAYUEUBUne/22rqRrUmEKmZTud9f07zXG6ZuOFmqoCAAAAAAAAAAAAAAAXpiuTbTyooKFtsrVDuUTtnVPZnCuWqG2f3/cQShV24wSbNtM0xVuhIep6DDQ6k+mV68oEzoW5R1EYiwq1sYMNSWeXTKG+NcqpxqccJVPYfd8WXZ1wlEShHpb7ovM/L7RIYbPeihMuHIlCEz0t7GGIwwUKQ/Rw3j9y777vtj8SuVLOZzjuLfY9Iudhuw6x2f0PP33iNlMUmeG6zP12kb8D1bZI5n44qMes9nsWv5THSULPlNCq3rV28/HdP6+gaKHFdg660seLREWJ4oWHD/32D4R1ilcp3Ac+p3iVwkPgmihZqG07/+v+InBZaMnC4Nx8ab8MnKcoWGinLU5tzgPnfYZYYZgjTpZYvtAuFXV3/h5CsFDb5y72zYsSwcKQ9v5GrjCa4DULQ1qfXKFJfkUnVZh4EkoWpgZSeF74j1a5lrqWbbLqOjvYcxTGpkIVct+025KFj31W7ldSruC3FdOCfST/1PiUvr3bDJ5xL14y/l2i7epsXdp3Br+VWPB3AQAAAAAAAAAAAACAIr4AdboYA+fBeQsAAAAASUVORK5CYII="
+                    }
+                    alt="Image not found"
+                  />
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </>
   );
